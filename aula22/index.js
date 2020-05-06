c36c2509b205c2f592d56a487bdab7a33e0157b9
jos3s <karatedkgt17@gmail.com>
2020-05-06 14:59:04 -0300

    Adição/Operadores lógicos

---
+++ aula22/index.js
@@ -0,0 +1,19 @@
+// Operadores lógicos
+
+//* && => And -> E
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log();
+
+//* || => Or -> Ou
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false); 
+console.log();
+
+//* ! => Not -> Não
+console.log(!true);
+console.log(!false);
+console.log();
