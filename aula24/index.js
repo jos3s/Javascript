e6f865575619ba75661e21648af404d7a36be70d
jos3s <karatedkgt17@gmail.com>
2020-05-06 20:17:09 -0300

    Adição/Condicionais

---
+++ aula24/index.js
@@ -0,0 +1,21 @@
+//Condicional 
+
+const hora=20;
+
+if(hora > 0 && hora <=11){
+    console.log('Bom dia!');
+}else if(hora>11 && hora <=17){
+    console.log('Boa tarde!');
+}else if(hora>17 && hora<=24){
+    console.log('Boa noite!');
+}else{
+    console.log('Olá');
+}
+
+const numero=17;
+
+if(numero>=0 && numero <=5){
+    console.log('O número está entre 0 e 5.');
+}else{
+    console.log('O número não está entre 0 e 5.');
+}
