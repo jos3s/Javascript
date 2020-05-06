56c71e454e24b4f0e780baf36b74ab373cc219d4
jos3s <karatedkgt17@gmail.com>
2020-05-06 14:58:38 -0300

    Adição/Usando operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,28 @@
+//Operadores de comparação
+
+//* >
+console.log(10>5);
+
+//* <
+console.log(10<5);
+
+//* >=
+console.log(10>=5);
+
+//* <=
+console.log(10<=5);
+
+//* ==
+console.log(10==5);
+console.log(10=='10'); //Funciona mas não deveria
+
+//* !=
+console.log(10!=5);
+
+//* ===
+console.log(10===5);
+console.log(10==='10');
+
+//* !==
+console.log(10!==5);
+
