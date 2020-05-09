b195fcd50af31113e68fe46442531c42cdfc29ec
jos3s <karatedkgt17@gmail.com>
2020-05-09 12:47:31 -0300

    Modificação/Adicionando as datas aos itens

--- aula48/js/main.js
+++ aula48/js/main.js
@@ -10,7 +10,7 @@ entrada.addEventListener('keypress', function (el) {
 window.addEventListener('click', function(el){
     e=el.target;
     if(e.classList.contains('remover')){
-        e.parentElement.remove();
+        e.parentElement.parentElement.remove();
         salvarTarefas();
     }
     if(e.classList.contains('adicionar')){
@@ -23,9 +23,12 @@ function criarElemento(e, txt=''){
     elemento.textContent=txt;
     return elemento;
 }
-function criarTarefa(txt){
-    const li=criarElemento('li',txt);
-    criarBotao(li);
+function criarTarefa(txt, data=''){
+    const li=criarElemento('li',txt+' ');
+    const div=criarElemento('div', '');
+    criarData(div, data);
+    criarBotao(div);
+    li.appendChild(div);
     tarefas.appendChild(li);
     limparInput();
     salvarTarefas();
@@ -39,6 +42,16 @@ function criarBotao(li) {
     apagar.appendChild(i);
     li.appendChild(apagar);
 }
+function criarData(li, data='') {
+    if(data!==''){
+        const small=criarElemento('small',data);
+        li.appendChild(small)
+    }else{
+        let d=new Date();
+        const small=criarElemento('small', d.toLocaleDateString());
+        li.appendChild(small)
+    }
+}
 function limparInput() {
     entrada.value='';
     entrada.focus();
@@ -46,21 +59,33 @@ function limparInput() {
 function salvarTarefas() {
     const liTarefas=tarefas.querySelectorAll('li');
     const listaTarefas=[];
+    const listaData=[];
     for(let tarefa of liTarefas){
         listaTarefas.push(formatarTexto(tarefa));
+        listaData.push(formatarData(tarefa));
     }
+    const datasJSON=JSON.stringify(listaData);
+    localStorage.setItem('datas',datasJSON);
     const tarefasJSON=JSON.stringify(listaTarefas);
     localStorage.setItem('tarefas',tarefasJSON);
-};
-function formatarTexto(txt) {
+}
+function formatarTexto(txt){
     let texto=txt.innerText;
-    return texto.replace('Apagar','').trim();
+    const final=texto.indexOf('/');
+    return texto.slice(0,final-2).trim();
+}
+function formatarData(data){
+    let texto=data.innerText;
+    const final=texto.slice(texto.indexOf('/')-2, texto.lastIndexOf('/')+5);
+    return final;
 }
 function adicionaTarefasSalvas() {
     const tarefas=localStorage.getItem('tarefas');
     const listaTarefas=JSON.parse(tarefas);
-    for(let tarefa of listaTarefas){
-        criarTarefa(tarefa);
+    const datas=localStorage.getItem('datas');
+    const listaDatas=JSON.parse(datas);
+    for(let i=0;i<listaTarefas.length;i++){
+        criarTarefa(listaTarefas[i], listaDatas[i]);
     }
 }
 adicionaTarefasSalvas();
