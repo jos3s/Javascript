bc4e68b414ef44b97d77e9ddc8357c57e93ace25
jos3s <karatedkgt17@gmail.com>
2020-05-11 16:08:37 -0300

    Adição/Object.defineProperty() e Object.defineProperties()

---
+++ aula70-79/aula71.js
@@ -0,0 +1,30 @@
+// Object.defineProperty() e Object.defineProperties()
+
+function Produto(nome,preco,estoque) {
+    //@ Configura várias propriedade
+    Object.defineProperties(this,{
+        nome:{
+            enumerable  :true,   //mostra ou esconde a chave
+            value       :nome,  //valor ou função
+            writable    :true,   //pode alterar o valor ou não
+            configurable:true,  //reconfigurar a chave ou apagar a chave
+        },
+        preco:{
+            enumerable  :true,  //mostra ou esconde a chave
+            value       :preco,  //valor ou função
+            writable    :true,  //pode alterar o valor ou não
+            configurable:true,  //reconfigurar a chave ou apagar a chave
+        },
+    });
+
+    //@ Configura apenas uma propriedade
+    Object.defineProperty(this,'estoque',{
+        enumerable  :true,      //mostra ou esconde a chave
+        value       :estoque,   //valor ou função
+        writable    :true,      //pode alterar o valor ou não
+        configurable:false,     //reconfigurar a chave ou apagar a chave
+    });
+}
+const p1=new Produto('Camiseta',20,3);
+console.log(p1);
+console.log(Object.keys(p1));
