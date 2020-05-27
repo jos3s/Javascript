ec146e282a4a4a3e3ebe1f6eaa56fc2e50c3d6f1
jos3s <karatedkgt17@gmail.com>
2020-05-27 15:18:20 -0300

    Modificação/Criando pagina de cadastro e login e a possibilidade de criar uma conta

--- projetoAgenda/routes.js
+++ projetoAgenda/routes.js
@@ -9,6 +9,7 @@ route.get('/', homeController.index);
 
 // Rotas de login
 route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
 
 module.exports = route;
--- projetoAgenda/src/middlewares/middleware.js
+++ projetoAgenda/src/middlewares/middleware.js
@@ -1,9 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
-  next();
-};
-
-exports.outroMiddleware = (req, res, next) => {
+  res.locals.errors = req.flash('errors');
+  res.locals.success = req.flash('success');
   next();
 };
 
