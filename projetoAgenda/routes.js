7b7874af4423079f89faefce809dbc4e4b426188
jos3s <karatedkgt17@gmail.com>
2020-05-28 01:38:26 -0300

    Modificação/Criando contato e exibindo a visualização de edição

--- projetoAgenda/routes.js
+++ projetoAgenda/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/ContatoController');
+
+const {loginRequire} = require('./src/middlewares/middleware');
 
 // Rotas da home
 route.get('/', homeController.index);
-
 
 // Rotas de login
 route.get('/login/index', loginController.index);
@@ -13,5 +15,9 @@ route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
 
+//Rotas de contato
+route.get('/contato/index', loginRequire ,contatoController.index);
+route.post('/contato/register', loginRequire ,contatoController.register);
+route.get('/contato/index/:id', loginRequire, contatoController.editIndex);
 
 module.exports = route;
--- projetoAgenda/src/middlewares/middleware.js
+++ projetoAgenda/src/middlewares/middleware.js
@@ -16,3 +16,12 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequire=(req,res,next)=>{
+  if(!req.session.user){
+    req.flash("errors",'Você precisa estar logado');
+    req.session.save(()=>res.redirect('/'));
+    return;
+  }
+  next();
+}
